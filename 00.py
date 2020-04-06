08f3133516821d40cd676872386239fad1419701
chenai9734 <60611184+chenai9734@users.noreply.github.com>
2020-04-06 20:34:55 +0800

    增加时间匹配

--- 00.py
+++ 00.py
@@ -9,7 +9,7 @@ import json
 import difflib
 '''
 通过 日文名/中文名 字符串匹配（效果不好,两货标点乱用）
-通过 日文名/中文名 时间匹配（想法，待尝试）
+通过 日文名/中文名 时间匹配（效果不错）
 '''
 anidbkehuduan=""   #anidb申请的api客户端名称，默认版号为1(用于获取Anidn-API返回的XML数据)
 
@@ -35,42 +35,59 @@ def save_api_anidb(anidb):#保存API返回的XML数据
 
 def jiexi_Anidb_z(Anidb):#返回XML里的中文名
 	Anidb_xml="%s.xml" %(Anidb)
-	with open(Anidb_xml, "r",encoding='utf-8') as f:
-		data = f.read()
-		Anidb_z=re.findall(r'<title xml:lang="zh-Hans" type="official">(.*)</title>',data)[0]
-	f.close()
+	try:
+		with open(Anidb_xml, "r",encoding='utf-8') as f:
+			data = f.read()
+			Anidb_z=re.findall(r'<title xml:lang="zh-Hans" type="official">(.*)</title>',data)[0]
+		f.close()
+	except:
+		Anidb_z="未爬取"
+	
 	return Anidb_z
 
 def jiexi_Anidb_y(Anidb):#返回XML里的英文名
 	Anidb_xml="%s.xml" %(Anidb)
-	with open(Anidb_xml, "r",encoding='utf-8') as f:
-		data = f.read()
-		Anidb_y=re.findall(r'<title xml:lang="en" type="official">(.*)</title>',data)[0]
-	f.close()
+	try:
+		with open(Anidb_xml, "r",encoding='utf-8') as f:
+			data = f.read()
+			Anidb_y=re.findall(r'<title xml:lang="en" type="official">(.*)</title>',data)[0]
+		f.close()
+	except:
+		Anidb_y="未爬取"
 	return Anidb_y
 
 def jiexi_Anidb_r(Anidb):#返回XML里的日文名
 	Anidb_xml="%s.xml" %(Anidb)
-	with open(Anidb_xml, "r",encoding='utf-8') as f:
-		data = f.read()
-		Anidb_r=re.findall(r'<title xml:lang="ja" type="official">(.*)</title>',data)[0]
-	f.close()
+	try:
+		with open(Anidb_xml, "r",encoding='utf-8') as f:
+			data = f.read()
+			Anidb_r=re.findall(r'<title xml:lang="ja" type="official">(.*)</title>',data)[0]
+		f.close()
+	except:
+		Anidb_r="未爬取"
 	return Anidb_r
 
 def jiexi_Anidb_l(Anidb):#返回XML里的罗马音
 	Anidb_xml="%s.xml" %(Anidb)
-	with open(Anidb_xml, "r",encoding='utf-8') as f:
-		data = f.read()
-		Anidb_l=re.findall(r'<title xml:lang="x-jat" type="main">(.*)</title>',data)[0]
-	f.close()
+	try:
+		with open(Anidb_xml, "r",encoding='utf-8') as f:
+			data = f.read()
+			Anidb_l=re.findall(r'<title xml:lang="x-jat" type="main">(.*)</title>',data)[0]
+		f.close()
+	except:
+		Anidb_l="未爬取"
 	return Anidb_l
 
-def jiexi_Anidb_time(Anidb):#返回XML里的时间
+def jiexi_Anidb_time(Anidb):#返回XML里的时间（年月）
 	Anidb_xml="%s.xml" %(Anidb)
-	with open(Anidb_xml, "r",encoding='utf-8') as f:
-		data = f.read()
-		Anidb_time=re.findall(r'<startdate>(.*)</startdate>',data)[0]
-	f.close()
+	try:
+		with open(Anidb_xml, "r",encoding='utf-8') as f:
+			data = f.read()
+			Anidb_time=re.findall(r'<startdate>(.*)</startdate>',data)[0]
+			Anidb_time=Anidb_time[:-3]
+		f.close()
+	except:
+		Anidb_time="未爬取"
 	return Anidb_time
 
 def jiexi_Anidb_haibao(Anidb):#返回XML里jpg ID
@@ -91,46 +108,49 @@ def jiexi_Anidb_haibao(Anidb):#返回XML里jpg ID
 def get_equal_rate_1(str1, str2):# 字符串对比
 	return difflib.SequenceMatcher(None, str1, str2).quick_ratio()
 
-def bgmtv(name_Z,name_Y,name_R):
+def bgmtv(name_Z,name_Y,name_R,name_shijian):
 	name_Z_mohu = "https://bgm.tv/subject_search/%s?cat=2" %(name_Z)#2
 	name_Y_mohu = "https://bgm.tv/subject_search/%s?cat=2" %(name_Y)#6
 	name_R_mohu = "https://bgm.tv/subject_search/%s?cat=2" %(name_R)#4
 	name_Z_jizu = "https://bgm.tv/subject_search/%s?cat=2&legacy=1" %(name_Z)#1
 	name_Y_jizu = "https://bgm.tv/subject_search/%s?cat=2&legacy=1" %(name_Y)#5
 	name_R_jizu = "https://bgm.tv/subject_search/%s?cat=2&legacy=1" %(name_R)#3
-	time.sleep(3)
-	idhtml = requests.get(name_Z_jizu,headers=ua_headers)
-	idhtml=idhtml.text
-	bgmid=re.findall(r'<a href="/subject/(.*)" class="l">',idhtml)#bgmid1
-	bgmid_name_Z_jizu=bgmid
-	if len(bgmid) != 1:
+	if name_Z != "未爬取":
 		time.sleep(3)
-		idhtml = requests.get(name_Z_mohu,headers=ua_headers)
+		idhtml = requests.get(name_Z_jizu,headers=ua_headers)
 		idhtml=idhtml.text
-		bgmid=re.findall(r'<a href="/subject/(.*)" class="l">',idhtml)#bgmid2
-		bgmid_name_Z_mohu=bgmid
-	if len(bgmid) != 1:
-		time.sleep(3)
-		idhtml = requests.get(name_R_jizu,headers=ua_headers)
-		idhtml=idhtml.text
-		bgmid=re.findall(r'<a href="/subject/(.*)" class="l">',idhtml)#bgmid3
-		bgmid_name_R_jizu=bgmid
-	if len(bgmid) != 1:
-		time.sleep(3)
-		idhtml = requests.get(name_R_mohu,headers=ua_headers)
-		idhtml=idhtml.text
-		bgmid=re.findall(r'<a href="/subject/(.*)" class="l">',idhtml)#bgmid4
-		bgmid_name_R_mohu=bgmid
-	if len(bgmid) != 1:
-		time.sleep(3)
-		idhtml = requests.get(name_Y_jizu,headers=ua_headers)
-		idhtml=idhtml.text
-		bgmid=re.findall(r'<a href="/subject/(.*)" class="l">',idhtml)#bgmid5
-	if len(bgmid) != 1:
-		time.sleep(3)
-		idhtml = requests.get(name_Y_mohu,headers=ua_headers)
-		idhtml=idhtml.text
-		bgmid=re.findall(r'<a href="/subject/(.*)" class="l">',idhtml)#bgmid6
+		bgmid=re.findall(r'<a href="/subject/(.*)" class="l">',idhtml)#bgmid1
+		bgmid_name_Z_jizu=bgmid
+		if len(bgmid) != 1:
+			time.sleep(3)
+			idhtml = requests.get(name_Z_mohu,headers=ua_headers)
+			idhtml=idhtml.text
+			bgmid=re.findall(r'<a href="/subject/(.*)" class="l">',idhtml)#bgmid2
+			bgmid_name_Z_mohu=bgmid
+	if name_R != "未爬取":
+		if len(bgmid) != 1:
+			time.sleep(3)
+			idhtml = requests.get(name_R_jizu,headers=ua_headers)
+			idhtml=idhtml.text
+			bgmid=re.findall(r'<a href="/subject/(.*)" class="l">',idhtml)#bgmid3
+			bgmid_name_R_jizu=bgmid
+		if len(bgmid) != 1:
+			time.sleep(3)
+			idhtml = requests.get(name_R_mohu,headers=ua_headers)
+			idhtml=idhtml.text
+			bgmid=re.findall(r'<a href="/subject/(.*)" class="l">',idhtml)#bgmid4
+			bgmid_name_R_mohu=bgmid
+	if name_Y != "未爬取":
+		if len(bgmid) != 1:
+			time.sleep(3)
+			idhtml = requests.get(name_Y_jizu,headers=ua_headers)
+			idhtml=idhtml.text
+			bgmid=re.findall(r'<a href="/subject/(.*)" class="l">',idhtml)#bgmid5
+		if len(bgmid) != 1:
+			time.sleep(3)
+			idhtml = requests.get(name_Y_mohu,headers=ua_headers)
+			idhtml=idhtml.text
+			bgmid=re.findall(r'<a href="/subject/(.*)" class="l">',idhtml)#bgmid6
 	#print(bgmid_name_Z_jizu)   #3
 	#print(bgmid_name_Z_mohu)   #4
 	#print(bgmid_name_R_jizu)   #1
@@ -147,6 +167,12 @@ def bgmtv(name_Z,name_Y,name_R):
 			if duibidu == 1.0:
 				bgmid.append(diffaa)
 				break
+			shijian_a=apiurltext['air_date']
+			shijian_a=shijian_a[:-3]
+			duibidushijian=get_equal_rate_1(shijian_a,name_shijian)
+			if duibidushijian == 1.0:
+				bgmid.append(diffaa)
+				break
 	if len(bgmid) != 1:
 		bgmid=[]
 		for diffaa in bgmid_name_R_mohu:
@@ -157,6 +183,12 @@ def bgmtv(name_Z,name_Y,name_R):
 			apiurltext = json.loads(apiurlhtml)
 			duibidu=get_equal_rate_1(apiurltext['name'],name_R)
 			if duibidu == 1.0:
+				bgmid.append(diffaa)
+				break
+			shijian_a=apiurltext['air_date']
+			shijian_a=shijian_a[:-3]
+			duibidushijian=get_equal_rate_1(shijian_a,name_shijian)
+			if duibidushijian == 1.0:
 				bgmid.append(diffaa)
 				break
 	if len(bgmid) != 1:
@@ -171,6 +203,12 @@ def bgmtv(name_Z,name_Y,name_R):
 			if duibidu == 1.0:
 				bgmid.append(diffaa)
 				break
+			shijian_a=apiurltext['air_date']
+			shijian_a=shijian_a[:-3]
+			duibidushijian=get_equal_rate_1(shijian_a,name_shijian)
+			if duibidushijian == 1.0:
+				bgmid.append(diffaa)
+				break
 	if len(bgmid) != 1:
 		bgmid=[]
 		for diffaa in bgmid_name_Z_mohu:
@@ -183,15 +221,24 @@ def bgmtv(name_Z,name_Y,name_R):
 			if duibidu == 1.0:
 				bgmid.append(diffaa)
 				break
+			shijian_a=apiurltext['air_date']
+			shijian_a=shijian_a[:-3]
+			duibidushijian=get_equal_rate_1(shijian_a,name_shijian)
+			if duibidushijian == 1.0:
+				bgmid.append(diffaa)
+				break
 	if len(bgmid) != 1:
 		bgmid=[]
 	return bgmid
 
-#print(bgmtv("我的妹妹哪有这么可爱.","Ore no Imouto ga Konna ni Kawaii Wake ga Nai.","俺の妹がこんなに可愛いわけがない."))
-save_api_anidb(13262)
-print(jiexi_Anidb_z(13262))
-print(jiexi_Anidb_y(13262))
-print(jiexi_Anidb_r(13262))
-print(jiexi_Anidb_l(13262))
-print(jiexi_Anidb_time(13262))
-print(jiexi_Anidb_haibao(13262))
+
+xml_cunzai=os.path.exists(r"7525.xml")
+if xml_cunzai != True:
+	save_api_anidb(7525)
+print(bgmtv(jiexi_Anidb_z(14471),jiexi_Anidb_y(14471),jiexi_Anidb_r(14471),jiexi_Anidb_time(14471)))
+#print(jiexi_Anidb_z(13262))
+#print(jiexi_Anidb_y(13262))
+#print(jiexi_Anidb_r(13262))
+#print(jiexi_Anidb_l(13262))
+#print(jiexi_Anidb_time(13262))
+#print(jiexi_Anidb_haibao(13262))
